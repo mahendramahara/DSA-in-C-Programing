90f60a79bbaabcc465ae1610315368bb9475b5bd
mahendramahara <mahendramahara369@gmail.com>
2022-01-09 07:00:00 +0545

    Added Data Structure & Algorithm in C-Programming 👨‍💻

---
+++ RECURSION/Factorial.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to calculate factorial using recursion
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } 
+	else {
+        unsigned long long result = factorial(num);
+        printf("Factorial of %d is %llu\n", num, result);
+    }
+    
+    return 0;
+}
+
