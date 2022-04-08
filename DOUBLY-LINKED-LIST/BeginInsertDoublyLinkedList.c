a4e0ce8e98851e5caa634cd28dca10e6c5bb78f2
mahendramahara <mahendramahara369@gmail.com>
2022-04-08 07:00:00 +0545

    Added Data Structure & Algorithm in C-Programming 👨‍💻

---
+++ DOUBLY-LINKED-LIST/BeginInsertDoublyLinkedList.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+void beginsert(int);
+struct node
+{
+	int data;
+	struct node *prev;
+	struct node *next;
+};
+struct node *head, *temp;
+void main()
+{
+	int choice,item;
+	do{
+		printf("Enter the item which you want to insert: \n");
+		scanf("%d", &item);
+		beginsert(item);
+		printf("\nPress 0 to insert more: \n");
+		scanf("%d", &choice);
+	}while(choice==0);
+	printf("\n\nItems in the list are:");
+    //Print items in the node
+    for (temp = head ; temp!=NULL ; temp=temp->next )
+	{
+    printf("%d\t",temp -> data );
+	}
+}
+void beginsert(int item)
+{
+	struct node *ptr = (struct node *)malloc(sizeof(struct node*));
+	if(ptr == NULL)
+	{
+		printf("\n ****************OVERFLOW**************** \n");
+	}
+	else
+	{
+		ptr->data = item;
+		ptr->next = head;
+		ptr->prev = NULL;
+		if (head != NULL)
+            head->prev = ptr;
+		head=ptr;
+		printf("\n ****************Node Inserted**************** \n\n");
+	}
+}
